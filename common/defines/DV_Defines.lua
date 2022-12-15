990022134ab06e1f777e495de0a1c11f09321546
TK2030 <63667726+martincheboludo@users.noreply.github.com>
2022-12-15 12:32:57 -0500

    Initial

---
+++ common/defines/DV_Defines.lua
@@ -0,0 +1,14 @@
+
+NDefines.NMilitary.CORPS_COMMANDER_DIVISIONS_CAP = 60            -- how many divisions a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.CORPS_COMMANDER_ARMIES_CAP = -1                -- how many armies a corps commander is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_DIVISIONS_CAP = 60            -- how many divisions a field marshall is limited to. 0 = inf, < 0 = blocked
+NDefines.NMilitary.FIELD_MARSHAL_ARMIES_CAP = 5                    -- how many armies a field marshall is limited to. 0 = inf, < 0 = blocked
+
+NDefines.NCountry.POLITICAL_POWER_LOWER_CAP = -500.0			-- Min amount of political power country should have
+NDefines.NCountry.POLITICAL_POWER_UPPER_CAP = 10000.0				-- Max amount of political power country should have
+NDefines.NMilitary.MAX_ARMY_EXPERIENCE = 2500
+NDefines.NMilitary.MAX_NAVY_EXPERIENCE = 2500
+NDefines.NMilitary.MAX_AIR_EXPERIENCE = 2500
+
+NDefines_Graphics.NFrontend.CAMERA_MIN_HEIGHT = 25.0
+NDefines_Graphics.NFrontend.CAMERA_MAX_HEIGHT = 4000.0
---
+++ descriptor.mod
@@ -0,0 +1,14 @@
+version="1.0"
+tags={
+	"Alternative History"
+	"National Focuses"
+	"Military"
+	"Technologies"
+	"Gameplay"
+	"Historical"
+	"Map"
+	"Ideologies"
+}
+name="Das_Vaterland"
+supported_version="1.12.7"
+replace_path="events"
